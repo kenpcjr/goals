002c5573fb2103c6a5c9cee0619c53c3f3df777b
kenpcjr <kennethcooke@me.com>
2016-08-07 09:12:11 -0400

    Implements pageview for settings

---
+++ Priority/SettingsTableView.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableView.swift
+//  Priority
+//
+//  Created by Kenneth Cooke on 8/6/16.
+//  Copyright © 2016 kencooke. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsTableView: UITableViewController {
+
+}
---
+++ Priority/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Priority
+//
+//  Created by Kenneth Cooke on 8/6/16.
+//  Copyright © 2016 kencooke. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: UIViewController {
+
+}

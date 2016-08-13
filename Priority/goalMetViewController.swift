8aa178ae30c4a6efa4fa8838a01f6d49ef4eea72
kenpcjr <kennethcooke@me.com>
2016-08-13 12:06:41 -0400

    Implements pod for currency textfield and begins success conditions

---
+++ Priority/goalMetViewController.swift
@@ -0,0 +1,13 @@
+//
+//  goalMetViewController.swift
+//  Priority
+//
+//  Created by Kenneth Cooke on 8/10/16.
+//  Copyright © 2016 kencooke. All rights reserved.
+//
+
+import Cocoa
+
+class goalMetViewController: UIViewController {
+
+}

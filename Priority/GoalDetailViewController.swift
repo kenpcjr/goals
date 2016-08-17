d549136f2932bed988b2bffc3f7ffe9358295268
kenpcjr <kennethcooke@me.com>
2016-08-17 08:34:22 -0400

    Sets segue logic for past goals detail view

---
+++ Priority/GoalDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GoalDetailViewController.swift
+//  Priority
+//
+//  Created by Kenneth Cooke on 8/17/16.
+//  Copyright © 2016 kencooke. All rights reserved.
+//
+
+import UIKit
+
+class GoalDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
